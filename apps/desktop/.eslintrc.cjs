73bcd81bd18fe5a79a99916449a3b46067c69751
ToastedToast <hey@toasted.dev>
2024-03-01 18:53:06 +0800

    feat(desktop): adjust config

--- apps/desktop/.eslintrc.cjs
+++ apps/desktop/.eslintrc.cjs
@@ -2,17 +2,36 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "no-undef": "off",
+    quotes: ["error", "double", { avoidEscape: true }],
+    semi: ["error", "always"],
+    "quote-props": ["error", "as-needed"],
+    "prefer-const": "error",
+    "no-var": "error",
+    "no-async-promise-executor": "off",
+    "@typescript-eslint/array-type": "error",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/consistent-type-assertions": "error",
+    "@typescript-eslint/consistent-type-imports": "error",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_",
+      },
+    ],
+    "@typescript-eslint/no-explicit-any": "off",
   },
-}
+};
