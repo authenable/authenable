d2dc58d97d9d1c7460a0785de04b2d697f8a5b2c
ToastedToast <hey@toasted.dev>
2024-03-01 19:05:21 +0800

    feat(desktop): init tanstack router

--- apps/desktop/src/main.tsx
+++ apps/desktop/src/main.tsx
@@ -1,27 +1,34 @@
-import React from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 import "./index.css";
+import { routeTree } from "./routeTree.gen";
 import { ClerkProvider } from "@clerk/clerk-react";
 
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+// Import your publishable key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
-    </ClerkProvider>
-  </React.StrictMode>,
-);
-
-// Remove Preload scripts loading
-postMessage({ payload: "removeLoading" }, "*");
-
-// Use contextBridge
-window.ipcRenderer.on("main-process-message", (_event, message) => {
-  console.log(message);
-});
+// Render the app
+const rootElement = document.getElementById("root")!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <RouterProvider router={router} />
+      </ClerkProvider>
+    </StrictMode>,
+  );
+}
