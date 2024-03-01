f9df6303664c35e3996c9e78fa3031a5b0c152f5
ToastedToast <hey@toasted.dev>
2024-03-01 18:58:28 +0800

    feat(desktop): auth

--- apps/desktop/src/App.tsx
+++ apps/desktop/src/App.tsx
@@ -1,5 +1,29 @@
+import {
+  SignOutButton,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+} from "@clerk/clerk-react";
+
 function App() {
-  return <p>hello world</p>;
+  return (
+    <>
+      <SignedOut>
+        <SignInButton />
+        <p>
+          This content is public. Only signed out users can see the SignInButton
+          above this text.
+        </p>
+      </SignedOut>
+      <SignedIn>
+        <SignOutButton />
+        <p>
+          This content is private. Only signed in users can see the
+          SignOutButton above this text.
+        </p>
+      </SignedIn>
+    </>
+  );
 }
 
 export default App;
